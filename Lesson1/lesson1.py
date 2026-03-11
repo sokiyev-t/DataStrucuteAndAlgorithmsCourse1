5a07eb7c3aaa5d3103c1d1433d575bf0a0f2b8ff
sokiyev-t <iron623@mail.ru>
2026-03-11 18:38:44 +0500

    First lesson

--- Lesson1/lesson1.py
+++ Lesson1/lesson1.py
@@ -1,4 +1,49 @@
+# a=7
+# b=9
+# c=a+b
+# print(c)
+
+# a=15
+# b=6
+
+# if a>b:
+#     print("a больше чем b")
+# else:
+#     print("b больше чем a")
+
+# print("Программа завершена")
 
 
+# a=10
+# b=6
+# c=3
 
-print("Hello, World!")
+# if a>b and a>c:
+#     m=a
+# elif b>a and b>c:
+#     m=b
+# else:    m=c
+
+# print(m)
+
+x=12.8
+a=5
+b=True
+c="Hello"
+print(type(x))
+print(type(a))
+print(type(b))
+print(type(c))
+
+# a=10
+# b=16
+# c=23
+
+
+# a,b,c=input("Введите три числа через пробел: ").split()
+# m=a
+# if b>m:
+#     m=b
+# if c>m:
+#     m=c
+# print(m)
